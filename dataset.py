05abb5ed1a4cab677f918a0f376dd53f67dafa8a
DenisKoposov <denis.koposov@phystech.edu>
2019-03-18 16:25:39 +0300

    dataset class for Synth90k

--- dataset.py
+++ dataset.py
@@ -4,6 +4,7 @@ from PIL import Image
 import torch
 from torch.utils.data import Dataset
 import numpy as np
+import matplotlib.pyplot as plt
 
 def extract_target(file_name):
     return file_name.split('_')[1]
@@ -12,7 +13,7 @@ class WordImageDataset(Dataset):
     """ Some documantation"""
 
     def __init__(self, root_dir, annotation_file, transform=None, preload=None,
-                 size=(32, 100)):
+                 size=(100, 32)):
         """
         Args:
         """
@@ -30,8 +31,9 @@ class WordImageDataset(Dataset):
         # Store all the data in memory to speed up computations. HUGE MEMORY CONSUMPTION!!!
         if preload:
             idx = np.random.choice(len(self.path), preload, replace=False)
-            self.images = [Image.open(pth).resize(self.size) for pth in self.path[idx]]
-            self.target = self.target[idx]
+            self.path = [self.path[i] for i in idx]
+            self.target = [self.target[i] for i in idx]
+            self.images = [Image.open(os.path.join(root_dir, pth)).resize(self.size) for pth in self.path]
 
 
     def __getitem__(self, idx):
@@ -39,9 +41,9 @@ class WordImageDataset(Dataset):
         if self.preload:
             img = self.images[idx]
         else:
-            img = Image.open(self.path[idx]).resize(self.size)
+            img = Image.open(os.path.join(self.root_dir, self.path[idx])).resize(self.size)
         
-        y = list(self.targets[idx])
+        y = list(self.target[idx])
         
         if not self.transform is None:
             img = self.transform(img)
@@ -60,3 +62,8 @@ if __name__ == "__main__":
     root_dir = "../synth90k"
     dataset = WordImageDataset(root_dir, "annotation_test.txt")
     print(len(dataset))
+    data = dataset.__getitem__(150)
+    
+    print(data[0], data[1])
+    plt.imshow(data[0])
+    plt.show()
