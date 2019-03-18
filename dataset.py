5b84195f68becfa726c30b1bbbaf68e21b1ef2dc
DenisKoposov <denis.koposov@phystech.edu>
2019-03-18 16:09:32 +0300

    torchvision dataset class for Synth90k

--- dataset.py
+++ dataset.py
@@ -1,32 +1,62 @@
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
+import os
+import glob
+from PIL import Image
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+
+def extract_target(file_name):
+    return file_name.split('_')[1]
 
 class WordImageDataset(Dataset):
     """ Some documantation"""
 
-    def __init__(self, csv_file, root_dir, transform=None, preload=False):
+    def __init__(self, root_dir, annotation_file, transform=None, preload=None,
+                 size=(32, 100)):
         """
         Args:
         """
-        data = pd.read_csv(csv_file)
-        self.path = data['path']
-        self.targets = data['y']
         self.root_dir = root_dir
+        self.path = []
         self.transform = transform
-    
+
+        with open(os.path.join(root_dir, annotation_file), "r") as f:
+            for line in f:
+                self.path.append(line.split(" ")[0])
+        
+        self.target = [extract_target(pth) for pth in self.path]
+        self.preload = preload
+        self.size = size
+        # Store all the data in memory to speed up computations. HUGE MEMORY CONSUMPTION!!!
+        if preload:
+            idx = np.random.choice(len(self.path), preload, replace=False)
+            self.images = [Image.open(pth).resize(self.size) for pth in self.path[idx]]
+            self.target = self.target[idx]
+
 
     def __getitem__(self, idx):
 
-        size = (32, 100)
-        img_path = os.path.join(self.root_dir, self.path[idx])
-        img = Image.open(img_path).resize(size)
-        y = self.targets[idx]
+        if self.preload:
+            img = self.images[idx]
+        else:
+            img = Image.open(self.path[idx]).resize(self.size)
+        
+        y = list(self.targets[idx])
         
         if not self.transform is None:
             img = self.transform(img)
 
-        return img, torch.tensor(y)
-    
+        return img, y
+
     
     def __len__(self):
-        return len(self.path)
+        if self.preload:
+            return len(self.images)
+        else:
+            return len(self.path)
+
+
+if __name__ == "__main__":
+    root_dir = "../synth90k"
+    dataset = WordImageDataset(root_dir, "annotation_test.txt")
+    print(len(dataset))
